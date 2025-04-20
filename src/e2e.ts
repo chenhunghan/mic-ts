4d24bc9b5fe4fdc2ad44a7aa93c7c9222af7d792
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-04-20 13:40:22 +0300

    feat: add end-to-end testing script using tsx and update package dependencies

---
+++ src/e2e.ts
@@ -0,0 +1,62 @@
+import fs from "fs";
+import mic from "./index.js";
+
+const micInstance = mic({
+  rate: "16000",
+  channels: "1",
+  debug: false,
+  exitOnSilence: 6,
+});
+const micInputStream = micInstance.getAudioStream();
+
+const outputFileStream = fs.createWriteStream("output.raw");
+
+micInputStream.pipe(outputFileStream);
+
+let chunkCounter = 0;
+micInputStream.on("data", function (data) {
+  console.log(
+    "Recieved Input Stream of Size %d: %d",
+    data.length,
+    chunkCounter++,
+  );
+});
+
+micInputStream.on("error", function (err) {
+  console.log("Error in Input Stream: " + err);
+});
+
+micInputStream.on("startComplete", function () {
+  console.log("Got SIGNAL startComplete");
+  setTimeout(function () {
+    micInstance.pause();
+  }, 5000);
+});
+
+micInputStream.on("stopComplete", function () {
+  console.log("Got SIGNAL stopComplete");
+});
+
+micInputStream.on("pauseComplete", function () {
+  console.log("Got SIGNAL pauseComplete");
+  setTimeout(function () {
+    micInstance.resume();
+  }, 5000);
+});
+
+micInputStream.on("resumeComplete", function () {
+  console.log("Got SIGNAL resumeComplete");
+  setTimeout(function () {
+    micInstance.stop();
+  }, 5000);
+});
+
+micInputStream.on("silence", function () {
+  console.log("Got SIGNAL silence");
+});
+
+micInputStream.on("processExitComplete", function () {
+  console.log("Got SIGNAL processExitComplete");
+});
+
+micInstance.start();
