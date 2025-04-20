cf4b64aa798ccc2c9f2eaf90b15189f8355b7337
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-04-20 16:08:12 +0300

    fix: update default annotations in MicOptions interface for consistency

--- src/index.ts
+++ src/index.ts
@@ -21,31 +21,32 @@ interface MicOptions {
   encoding?: "signed-integer" | "unsigned-integer";
   /**
    * Sample rate of audio
-   * @default 16000
+   * @defaultValue `16000`
    */
   rate?: string;
   /**
    * Number of channels of audio data; e.g. 2 = stereo
-   * @default 1
+   * @defaultValue `1`
    */
   channels?: string;
   /**
    * arecord device
-   * @default plughw:1,0
+   * @defaultValue `plughw:1,0`
    */
   device?: string;
   /**
    * signal is raised after reaching these many consecutive frames
-   * @default: 0
+   * @defaultValue `0`
    */
   exitOnSilence?: number;
   /**
    * File type of audio, one of 8svx aif aifc aiff aiffc al amb au avr caf cdda cdr cvs cvsd cvu dat dvms f32 f4 f64 f8 fap flac fssd gsm gsrt hcom htk ima ircam la lpc lpc10 lu mat mat4 mat5 maud mp2 mp3 nist ogg opus paf prc pvf raw s1 s16 s2 s24 s3 s32 s4 s8 sb sd2 sds sf sl sln smp snd sndfile sndr sndt sou sox sph sw txw u1 u16 u2 u24 u3 u32 u4 u8 ub ul uw vms voc vorbis vox w64 wav wavpcm wve xa xi
-   * @default raw
+   * @defaultValue `raw`
    */
   fileType?: string;
   /**
    * Print debug info
+   * @defaultValue `false`
    */
   debug?: boolean;
 }
